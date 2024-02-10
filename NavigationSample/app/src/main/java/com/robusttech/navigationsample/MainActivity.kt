b9b164883cb86c79af8d19fef5a6b2603ed8bd43
Yunus Olgun <y.olgun@gmail.com>
2024-02-10 20:23:06 +0300

    NavigationSample added

---
+++ NavigationSample/app/src/main/java/com/robusttech/navigationsample/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.robusttech.navigationsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.robusttech.navigationsample.ui.theme.NavigationSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationSampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "firstscreen") {
+        composable("firstscreen") {
+            FirstScreen {name->
+                navController.navigate("secondscreen/$name")
+            }
+        }
+        composable(route = "secondscreen/{name}") {
+            val name = it.arguments?.getString("name") ?: "no name"
+            SecondScreen(name) {
+                navController.navigate("firstscreen")
+            }
+        }
+    }
+}
---
+++ NavigationSample/app/src/main/java/com/robusttech/navigationsample/SecondScreen.kt
@@ -0,0 +1,47 @@
+package com.robusttech.navigationsample
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SecondScreen(name: String, navigateToFirstScreen:(String)->Unit) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("This is the Second Screen", fontSize = 24.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text("Welcome $name", fontSize = 24.sp)
+        Button(onClick = {
+            navigateToFirstScreen(name)
+        }) {
+            Text("Go to First Screen")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SecondPreview() {
+    SecondScreen("Test", {})
+}
+
