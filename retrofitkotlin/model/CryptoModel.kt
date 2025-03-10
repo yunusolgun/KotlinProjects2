a661610f04a798236aff430c28f30d7e09262454
yunusolgun <71584995+yunusolgun@users.noreply.github.com>
2025-03-10 12:52:06 +0300

    retrofit and rxjava - async download from internet

---
+++ retrofitkotlin/model/CryptoModel.kt
@@ -0,0 +1,12 @@
+package com.robusttech.retrofitkotlin.model
+
+import com.google.gson.annotations.SerializedName
+
+data class CryptoModel(
+    @SerializedName("currency")
+    val currency: String,
+
+    @SerializedName("price")
+    val price: String
+) {
+}
